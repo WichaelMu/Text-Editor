c490dd7fb47956bf7a1b40a8bc7a3e973e3f4987
WichaelMu <69779024+WichaelMu@users.noreply.github.com>
2022-10-08 19:11:24 +1100

    Cut, Copy & Paste

--- Forms/Text Editor/FEditorCCP.cs
+++ Forms/Text Editor/FEditorCCP.cs
@@ -7,16 +7,24 @@ namespace MTextEditor
 		void Cut()
 		{
 			Print(this, Cut, "Cut");
+
+			if (RTextArea.SelectionLength > 0)
+				RTextArea.Cut();
 		}
 
 		void Copy()
 		{
 			Print(this, Copy, "Copy");
+
+			if (RTextArea.SelectionLength > 0)
+				RTextArea.Copy();
 		}
 
 		void Paste()
 		{
 			Print(this, Paste, "Paste");
+
+			RTextArea.Paste();
 		}
 	}
 }
